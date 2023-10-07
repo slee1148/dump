f8125d50b1acc281182ce3dfccf9dbc068edb50a
slee1148 <135320739+slee1148@users.noreply.github.com>
2023-10-07 20:33:00 +0900

    Update tool.cpp

--- tool.cpp
+++ tool.cpp
@@ -6,13 +6,10 @@ int menu(){
     cout << "0, 1, 2,에서 고르시오.";
     cin >> a;
     if (a == 0 || a == 1 || a == 2)
-      break;
+      return a;
     else
       cout << "0, 1, 2 에서 고르시오";
   }
-  if (a == 1) //학사팀으로 ㄱㄱ
-  else if (a == 2) //학생팀으로 ㄱ
-  else if (a == 0) return 0; 
 }
 
 int main(){
@@ -20,6 +17,10 @@ int main(){
     int a = menu();
     if (a == 0)
       break;
+    else if (a == 1)
+      //학사팀으로 ㄱ
+    else if (a == 2)
+      //학생팀으로 ㄱ
   }
 
   return 0;
